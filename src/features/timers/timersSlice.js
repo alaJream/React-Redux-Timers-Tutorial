842c982f774b692f5f686a1a5de555c350623851
alaJream <jeremy.gonzales@students.dominican.edu>
2023-02-14 01:59:38 -0800

    added timer actions

--- src/features/timers/timersSlice.js
+++ src/features/timers/timersSlice.js
@@ -6,14 +6,22 @@ const initialState = {
 }
 
 export const timersSlice = createSlice({
-  name: 'timers',
-  initialState,
-  reducers: {
-    
-  },
-})
+    name: 'timers',
+    initialState,
+    reducers: {
+  
+      addTimer: (state, action) => {
+        state.value.push(new Timer(action.payload))
+      },
+  
+          toggleTimer: (state, action) => {
+              state.value[action.payload].isRunning = !state.value[action.payload].isRunning
+          },
+          
+    },
+  })
 
 // Action creators are generated for each case reducer function
-export const {  } = timersSlice.actions
+export const { addTimer, toggleTimer } = timersSlice.actions
 
 export default timersSlice.reducer
